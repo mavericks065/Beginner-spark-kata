bce0e2c471a6e7f82d7881b45f77228df259e8fc
nicolas <nicolas065@gmail.com>
2018-11-11 16:19:20 +1100

    fizz buzz solution

--- src/main/scala/au/com/training/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/au/com/training/fizzbuzz/FizzBuzz.scala
@@ -2,6 +2,11 @@ package au.com.training.fizzbuzz
 
 object FizzBuzz {
 
-  def getResult(number: Int): String = ???
-
+  def getResult(number: Int): String = if (number % 15 == 0)
+    "fizzbuzz"
+  else if (number % 3 == 0)
+    "fizz"
+  else if (number % 5 == 0)
+    "buzz"
+  else number.toString
 }
--- src/test/scala/au/com/training/fizzbuzz/FizzBuzzTest.scala
+++ src/test/scala/au/com/training/fizzbuzz/FizzBuzzTest.scala
@@ -6,34 +6,46 @@ class FizzBuzzTest extends FlatSpec with Matchers {
 
   it should "return fizz if the number is dividable by 3" in {
     // GIVEN
+    val number = 6
 
     // WHEN
+    val result = FizzBuzz.getResult(number)
 
     // THEN
+    result shouldEqual "fizz"
   }
 
-  it should "return buzz if the number is dividable by 5" ignore {
+  it should "return buzz if the number is dividable by 5" in {
     // GIVEN
+    val number = 10
 
     // WHEN
+    val result = FizzBuzz.getResult(number)
 
     // THEN
+    result shouldEqual "buzz"
   }
 
-  it should "return fizzbuzz if the number is dividable by 15" ignore {
+  it should "return fizzbuzz if the number is dividable by 15" in {
     // GIVEN
+    val number = 15
 
     // WHEN
+    val result = FizzBuzz.getResult(number)
 
     // THEN
+    result shouldEqual "fizzbuzz"
   }
 
-  it should "return the number if the number is not dividable by 3/5/15" ignore {
+  it should "return the number if the number is not dividable by 3/5/15" in {
     // GIVEN
+    val number = 257
 
     // WHEN
+    val result = FizzBuzz.getResult(number)
 
     // THEN
+    result shouldEqual "257"
   }
 
 }
